b300935f1e27fb081dbbd0e6f658699ce2217bdd
Charles・S <liangyingliang767@gmail.com>
2026-07-27 12:12:15 +0900

    メイン処理

---
+++ src/Calculater.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		double num1 = InputNumber1.getNumber(scanner);
+		String op = OperatorInput.getOperator(scanner);
+		double num2 = InputNumber2.getNumber(scanner);
+
+		double result = 0;
+
+		switch (op) {
+		case "+":
+			result = num1 + num2;
+			break;
+		case "-":
+			result = num1 - num2;
+			break;
+		case "*":
+			result = num1 * num2;
+			break;
+		case "/":
+			if (num2 != 0) {
+				result = num1 / num2;
+			} else {
+				System.out.println("エラー: 0で割ることはできません。");
+				scanner.close();
+				return;
+			}
+			break;
+		default:
+			System.out.println("エラー: 無効な演算子です。");
+			scanner.close();
+			return;
+		}
+
+		System.out.println("計算結果: " + result);
+		scanner.close();
+	}
+}
