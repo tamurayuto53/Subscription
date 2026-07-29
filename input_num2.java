985506a1224274b4a3b9b2b0e47a0c5f6064c8e2
tamura yuto <tamu@ip-10-95-3-109.ap-northeast-1.compute.internal>
2026-07-29 13:41:01 +0900

    num2

--- input_num2.java
+++ input_num2.java
@@ -1,6 +1,34 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class input_num2 {
-	Scanner scanner = new Scanner(System.in);
-	public int num2 = scanner.nextInt();
-}
+
+  Scanner scanner = new Scanner(System.in);
+  Random random = new Random();
+
+  public int num2;
+
+  public input_num2() {
+
+    System.out.println("2つ目の数字の入力方法を選択してください");
+    System.out.println("1: 手入力");
+    System.out.println("2: ランダム(1~100)");
+
+    int select = 0;
+    do {
+        select = scanner.nextInt();
+    } while(select != 1 && select != 2);
+    
+
+    if (select == 1) {
+      System.out.print("2つ目の数字を入力してください：");
+
+      num2 = scanner.nextInt();
+    } else {
+
+      num2 = random.nextInt(100) + 1;   
+      System.out.println("ランダムで生成された数字：" + num2);
+    }
+  }
+  
+} 
